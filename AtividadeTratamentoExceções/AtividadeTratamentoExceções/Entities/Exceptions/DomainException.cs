3dd3278b5533504bace0862ec666452aa2f9171e
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2023-12-05 16:23:26 -0300

    Adding AtividadeTratamentoExceções

---
+++ AtividadeTratamentoExceções/AtividadeTratamentoExceções/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeTratamentoExceções.Entities.Exceptions {
+    class DomainException : ApplicationException{
+        public DomainException(string message) : base(message) { }
+    }
+}
